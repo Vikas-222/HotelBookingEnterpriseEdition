73543f7387e622c3a31047f30b32c5bec01e3027
vikas <vsa@narola.email>
2025-04-08 17:25:34 +0530

    cancel booking api

--- src/main/java/com/example/controller/CalculateRevenueController.java
+++ src/main/java/com/example/controller/CalculateRevenueController.java
@@ -1,6 +1,15 @@
 package com.example.controller;
 
 import com.example.common.AppConstants;
+import com.example.common.Messages;
+import com.example.common.Response;
+import com.example.common.exception.ApplicationException;
+import com.example.common.exception.DBException;
+import com.example.common.utils.CustomObjectMapper;
+import com.example.common.utils.SessionValidator;
+import com.example.dto.UserDTO;
+import com.example.service.BookingService;
+import com.mysql.cj.protocol.Message;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,7 +22,31 @@ import java.io.IOException;
 public class CalculateRevenueController extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType(AppConstants.APPLICATION_JSON);
+        BookingService service = new BookingService();
+        try{
+            UserDTO user = SessionValidator.checkSession(request);
+            if(!user.getRole().equalsIgnoreCase("admin")){
+                throw new ApplicationException(Messages.Error.UNAUTHORIZED_ACCESS);
+            }
+            Float revenue = service.calculateRevenue();
+            sendResponse(response,null,null,revenue,200);
+        }catch (DBException e) {
+            e.printStackTrace();
+            sendResponse(response,Messages.Error.FAILED,e.getMessage(),null,500);
+        } catch (ApplicationException e) {
+            e.printStackTrace();
+            sendResponse(response,e.getMessage(),null,null,400);
+        }
+    }
+
+    private void sendResponse(HttpServletResponse response, String message, String technicalMessage, Object data, int statusCode) throws IOException {
+        response.setStatus(statusCode);
+        Response apiResponse = new Response();
+        apiResponse.setMessage(message);
+        apiResponse.setTechnicalMessage(technicalMessage);
+        apiResponse.setData(data);
+        response.getWriter().write(CustomObjectMapper.toString(apiResponse));
     }
 }
